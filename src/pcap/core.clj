c5f586267c2403549437912be2e53558bad8dbe5
Andrew Hobson <ahobson@damballa.com>
2015-07-26 17:02:25 -0400

    parse ip

--- src/pcap/core.clj
+++ src/pcap/core.clj
@@ -1,70 +1,66 @@
 (ns pcap.core
   (require [gloss (core :as glc) (io :as gio)]))
 
-(glc/defcodec file-header
-  (glc/ordered-map :magic   :uint32-le
-                   :major   :uint16-le
-                   :minor   :uint16-le
-                   :zone    :int32-le
-                   :sigfigs :uint32-le
-                   :snaplen :uint32-le
-                   :network link-type))
+(glc/defcodec ipv4-header
+  (glc/ordered-map :version-len  (glc/bit-map :version 4 :header-len 4)
+                   :tos          :ubyte
+                   :len          :uint16-be
+                   :id           :uint16-be
+                   :flags-offset (glc/bit-map :flags 3 :offset 13)
+                   :ttl          :ubyte
+                   :protocol     ip-protocols
+                   :checksum     :uint16-be
+                   :src_ip       :uint32-be
+                   :dst_ip       :uint32-be))
 
-(glc/defcodec link-type
-  (glc/enum :uint32-le {:null       0
-                        :ethernet   1
-                        :raw        101}))
+(defn ipv4-data-len
+  [h]
+  (let [hdr-len   (* 4 (get-in h [:version-len :header-len]))
+        total-len (:len h)]
+    (- total-len hdr-len)))
 
-(glc/defcodec packet-len
-  (glc/ordered-map :len     :uint32-le
-                   :pktlen  :uint32-le))
+(glc/defcodec ipv4-packet
+  (glc/header ipv4-header
+              (fn [h]
+                (glc/ordered-map :options glc/nil-frame
+                                 :payload (glc/finite-block (ipv4-data-len h))))
+              (fn [b]
+                (:header b))))
 
-(glc/defcodec ethernet-proto
-  (glc/enum :uint16-le {:ip 0x0800}))
+(glc/defcodec ethernet-protocol
+  (glc/enum :uint16 {:arp           0x0806
+                     :rarp          0x8035
+                     :ipv4          0x0800
+                     :lldp          0x88CC
+                     :bsn           0x8942
+                     :vlan-untagged 0xffff
+                     :ipv6          0x86dd}))
 
-(glc/defcodec ip-packet
-  (glc/ordered-map :hdr-len :uint8-le
-                   :version :uint8-le
-                   :tos     :uint8-le
-                   :len     :uint16-le
-                   :id      :uint16-le
-                   :off     :uint16-le
-                   :ttl     :uint8-le
-                   :proto   :uint8-le
-                   :chk     :uint16-le
-                   :src     :uint32-le
-                   :dst     :uint32-le))
-
-(def etherent-proto-map
-  {:ip ip-packet})
-
-(glc/defcodec ethernet-src-dst
-  (glc/ordered-map :dst   (glc/finite-block 6)
-                   :src   (glc/finite-block 6)))
+(glc/defcodec ethernet-header
+  (glc/ordered-map
+   :dst_addr          (glc/finite-block 6)
+   :src_addr          (glc/finite-block 6)
+   :ethernet-protocol ethernet-protocol))
 
 (glc/defcodec ethernet-packet
-  (glc/header ethernet-proto
+  (glc/header ethernet-header
               (fn [h]
-                (h ethernet-proto-map))))
+                ipv4-packet)
+              (fn [b]
+                (select-keys b [:dst_addr :src_addr :ethernet-protocol]))))
 
-(glc/defcodec packet
-  (glc/ordered-map :sec     :uint32-le
-                   :usec    :uint32-le
-                   :payload (glc/repeated
-                             :ubyte
-                             :prefix
-                             (glc/prefix packet-len
-                                         :len
-                                         (fn [b]
-                                           {:len (alength b)
-                                            :pktlen (alength b)})))))
+(glc/defcodec ip-protocols
+  (glc/enum :ubyte {:icmp  0x01
+                    :igmp  0x02
+                    :tcp   0x06
+                    :udp   0x11}))
 
-(glc/defcodec pcap
-  (glc/ordered-map :file-header    file-header
-                   :packets        (glc/repeated packet :prefix :none)))
+(glc/defcodec size-version
+  (glc/compile-frame :ubyte
+                     (fn [s]
+                       (bit-or (bit-shift-left (s :version) 4)
+                               (/ (s :size) 4 )))
+                     (fn [b]
+                       {:version (bit-shift-right b 4)
+                        :size (* 4  (bit-and b 0x0f))})))
 
-
-(defn parse
-  [filename]
-  (with-open [ins (io/input-stream (io/file filename))]
-    (gio/decode pcap ins)))
